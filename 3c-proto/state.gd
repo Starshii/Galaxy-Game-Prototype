4397f1177969b61f17c8498e10987aaf1bb0dc57
CannonBallBluey <starskruijsen@gmail.com>
2026-05-05 21:56:14 +0100

    reimported whole project
    it is now olivias version with also some rhythm stuff in there, basic animation player and song setup

---
+++ 3c-proto/state.gd
@@ -0,0 +1,21 @@
+@abstract class_name State extends RefCounted
+
+@abstract func check_state(this : Node) -> State
+@abstract func update(this : Node, delta : float)
+@abstract func on_enter(this : Node)
+@abstract func on_exit(this : Node)
+
+class NullState extends State:
+	var name := "NullState"
+	
+	func check_state(_this : Node) -> State:
+		return null
+	
+	func on_enter(_this : Node):
+		pass
+	
+	func on_exit(_this : Node):
+		pass
+	
+	func update(_this : Node, _delta : float):
+		pass
--- galaxy-game-prototype/cameraRaycast.gd
+++
@@ -1,7 +0,0 @@
-extends RayCast3D
-
-var hit
-func _physics_process(delta: float) -> void:
-	if is_colliding():
-		hit = get_collider()
-		
